cbf18713013a26d4c2a79eacadc3212231fb0466
ali <alirazamunir401@gmail.com>
2020-07-03 14:53:38 +0500

    finished  given task.

---
+++ src/components/Main.js
@@ -0,0 +1,67 @@
+import React from "react";
+import "../App.css";
+import pik from "../img/pik.png";
+import icon from "../img/icon.jpg"
+
+function Main() {
+  return (
+    <div>
+      <div className="container">
+
+        <p id="header" className="my-2">
+          GitHub <span id='header_light' className='clr-secondary'>Guides</span>
+        </p>
+
+        <div className="grid mt-6">
+          <div className="w-24 w-24@sm w-12@md column px-4 py-2">
+            <img src={pik} className="img txt-center" alt="" id="img_crd" />
+            <p className="mt-2">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae
+              doloribus et libero ut eius quos, aliquam atque necessitatibus
+              dolorum veritatis.
+            </p>
+            <small id="my_small_text">
+              <i className="ri-time-line"></i> <span id='sm_text_span'>5 minutes ago</span>
+            </small>
+          </div>
+
+          <div className="w-24 w-24@sm w-12@md column px-4 py-2">
+            <img src={pik} className="img txt-center" alt="" id="img_crd" />
+            <p className="mt-2">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae
+              doloribus et libero ut eius quos, aliquam atque necessitatibus
+              dolorum veritatis.
+            </p>
+            <small id="my_small_text">
+              <i className="ri-time-line"></i> <span id='sm_text_span'>5 minutes ago</span>
+            </small>
+          </div>
+
+          <div className="w-24 w-24@sm w-12@md column px-4 py-2">
+            <img src={pik} className="img txt-center" alt="" id="img_crd" />
+            <p className="mt-2">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae
+              doloribus et libero ut eius quos, aliquam atque necessitatibus
+              dolorum veritatis.
+            </p>
+            <small id="my_small_text">
+              <i className="ri-time-line"></i> <span id='sm_text_span'>5 minutes ago</span>
+            </small>
+          </div>
+        </div>
+
+        <div id="footer" className="txt-center clr-secondary">
+          <img src={icon} id='footer_icon' />
+          <p>
+            GitHub is the best way to build and ship software.</p>
+            <p>
+            Powerful collaboration, code review, and code management for open
+            source and private projects.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Main;
